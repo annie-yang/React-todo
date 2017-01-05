868a95b837821c03a17386b2b7ef32fca2ffea51
Annie Y <annie_y48@hotmail.com>
2017-01-05 11:58:15 -0800

    Style todo app

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -5,6 +5,12 @@ var TodoList = React.createClass({
   render: function(){
     var {todos} = this.props;
     var renderTodos = () => {
+      if(todos.length === 0){
+        return(
+          <p className="container_message"> Nothing To Do</p>
+        );
+      }
+      
       return todos.map((todo) => {
         return (
           // spread operator - takes every attribute from Todo and passes it down as a prop
